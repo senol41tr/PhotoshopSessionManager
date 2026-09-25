b9657739779c93fd6f394fd85918f76c5e08852a
M. Şenol Sarıgül <info@ssarigul.tr>
2026-09-25 07:01:19 +0300

    First Release

---
+++ PhotoshopSessionManager.jsx
@@ -0,0 +1,116 @@
+﻿#target photoshop
+
+// License: MIT
+// senol41tr (https://ssarigul.tr)
+// TESTED: CS 5.1 for Windows
+
+/*
+@@@BUILDINFO@@@ PhotoshopSessionManager.jsx 0.1
+*/
+
+var begDesc = "Open tabs from the previous session" // endDesc
+var begName = "Photoshop Session Manager (for Windows)" // endName
+var sessionFilePath = Folder.userData + "/photoshop_session_manager.files";
+
+function saveCurrentSession()
+{
+    try
+    {
+        var sessionFile = new File(sessionFilePath);
+        sessionFile.encoding = "UTF8";
+        sessionFile.open("w");
+        
+        for (var i = 0; i < app.documents.length; i++) {
+            try {
+                var docPath = app.documents[i].fullName.fsName;
+                sessionFile.writeln(docPath);
+            } catch (e) {
+            }
+        }
+        sessionFile.close();
+    } catch (err) {
+        alert("SessionManager Save Error: " + err.message);
+    }
+}
+
+function restoreSession() {
+    try {
+        var sessionFile = new File(sessionFilePath);
+        sessionFile.encoding = "UTF8";
+
+        if (!sessionFile.exists)
+        {
+            alert(sessionFilePath + ' not found!');
+            return;
+        }
+        
+        sessionFile.open("r");
+        while (!sessionFile.eof)
+        {
+            var filePath = sessionFile.readln();
+            if (filePath === "") continue;
+            
+            var fileToOpen = new File(filePath);
+            if (fileToOpen.exists)
+            {
+                try
+                {
+                    app.open(fileToOpen);
+                }
+                catch(e)
+                {
+                    alert("Failed to open document: " + filePath + "\nDetails: " + e.message);
+                }
+            }
+        }
+        sessionFile.close();
+    } catch (err) {
+        alert("SessionManager Restore Error: " + err.message);
+    }
+}
+
+
+var sessionFile = new File(sessionFilePath);
+sessionFile.encoding = "UTF8";
+
+if (!sessionFile.exists)
+{
+    // create .txt file
+    sessionFile.open("w");
+    sessionFile.close();
+
+    // add notifiers
+    try
+    {
+        var scriptFileObj = new File($.fileName);
+        if (scriptFileObj.exists)
+        {
+            // TODO check notifiers added
+            // app.notifiers.removeAll();
+            if(!app.notifiersEnabled) app.notifiersEnabled = true;
+            app.preferences.eventsReceipt = true;
+            app.notifiers.add('Ntfy', scriptFileObj); // Application Start
+            app.notifiers.add('Opn ', scriptFileObj); // Document Open
+            app.notifiers.add('Cls ', scriptFileObj); // Document Close
+            app.notifiers.add('save', scriptFileObj); // Document Save
+            alert("Photoshop Session Manager Installation Successfully!");
+        }
+    }
+    catch (err)
+    {
+        alert('Notifier Error: ' + err.message);
+    }
+}
+else
+{
+    var activeEvent = app.typeIDToStringID(arguments[1]);
+    
+    if (activeEvent === 'notify')
+    {
+        restoreSession();
+    }
+    else
+    {
+        saveCurrentSession();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# PhotoshopSessionManager
-Photoshop Session Manager (for Windows)
+# Photoshop Session Manager (for Windows)
+
+## Open tabs from the previous session
+
+**License** MIT
+
+**Author** senol41tr (https://ssarigul.tr)
+
+**Tested on** CS 5.1 and CC 2026 (27.10.0) for Windows
+
+
+### Installation
+
+```markdown
+1. Open Photoshop
+2. File > Scripts > Browse...
+3. Locate PhotoshopSessionManager.jsx
+4. Click Load Button
+5. If you saw the success message, it installed successfully.
+   Your sessions are now being recorded in the background.
+```
+
+### Uninstall
+
+```markdown
+1. Open Photoshop
+2. File > Scripts > Script Events Manager
+3. Remove all events with PhotoshopSessionManager.jsx
+4. Click Done Button
+5. Search and delete the "photoshop_session_manager.files" file or 
+   go to C:\Users\\[USERNAME]\AppData\Roaming\ and delete 
+   photoshop_session_manager.files file.
+```
